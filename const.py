9906a43712b24ca018aad5123b4c65e221f27eab
Christopher Leonard <c.leonard@iwoca.co.uk>
2026-08-14 10:42:26 +0000

    Add chapter 5 notes on posterior predictive checks with dummy data

--- const.py
+++ const.py
@@ -6,5 +6,6 @@ ROOT = Path(__file__).parent
 DATA_DIR = ROOT / "data"
 
 CHEMICAL_SHIFTS_DATA_PATH = DATA_DIR / "chemical_shifts_theo_exp.csv"
+DUMMY_DATA_PATH = DATA_DIR / "dummy.csv"
 
-RANDOM_SEED = 7
+RANDOM_SEED = 29375
